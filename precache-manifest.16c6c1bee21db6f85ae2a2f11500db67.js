3a75c528e00830b8fdc441206d3631329f775e5e
Anshul Dubey <anshuldubey2166@gmail.com>
2019-05-27 00:45:31 +0530

    Updates

---
+++ precache-manifest.16c6c1bee21db6f85ae2a2f11500db67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "343210a0004cd65468bf",
+    "url": "/Meteorite-Explorer-App/static/js/runtime~main.d855960d.js"
+  },
+  {
+    "revision": "637f0f8e999bf8f3f88c",
+    "url": "/Meteorite-Explorer-App/static/js/main.20907053.chunk.js"
+  },
+  {
+    "revision": "9117bb8e802b39b765a0",
+    "url": "/Meteorite-Explorer-App/static/js/2.499548e1.chunk.js"
+  },
+  {
+    "revision": "637f0f8e999bf8f3f88c",
+    "url": "/Meteorite-Explorer-App/static/css/main.53773101.chunk.css"
+  },
+  {
+    "revision": "9117bb8e802b39b765a0",
+    "url": "/Meteorite-Explorer-App/static/css/2.5ad9f5a9.chunk.css"
+  },
+  {
+    "revision": "2121483b250fdba31ef3f9eb525161ca",
+    "url": "/Meteorite-Explorer-App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Meteorite-Explorer-App/precache-manifest.16c6c1bee21db6f85ae2a2f11500db67.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Meteorite-Explorer-App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
